fea5480484ddc0b7a865ea67b7c99bdafde6bf3b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-05 15:57:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,12 +16,7 @@ class Scraper
     end 
     
     def get_courses
-      self.get_page.css(".posts-holder").each do |post|
-        course = Course.new
-        course.title = post.css("h2").text
-        course.schedule = post.css(".date").text
-        course.description = post.css("p").text
-      end
+      self.get_page.css(".posts-holder")
     end 
     
     def make_courses
