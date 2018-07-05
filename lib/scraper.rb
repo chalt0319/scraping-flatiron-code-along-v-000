7ba623dcd95f417a3af192d5457be94a872e968d
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-05 15:30:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,11 @@ require_relative './course.rb'
 class Scraper
     def get_page 
       html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-      @doc = Nokogiri::HTML(html)
+      doc = Nokogiri::HTML(html)
     end 
     
     def get_courses
-      @doc.css(".posts-holder")
+      self.get_page.css(".posts-holder")
     end 
     
     def make_courses
